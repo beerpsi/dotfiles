6f873dabf8e71240b43bdfff7fac095be49296a6
beerpsi <vaythuonggialac@tutanota.com>
2020-07-19 16:49:18 +0700

    first commit

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,7 @@
+export PATH="$HOME/Downloads/git/void-packages:$HOME/.config/scripts:$PATH"
+
+alias xi="sudo xbps-install"
+alias xr="sudo xbps-remove"
+alias xq="xbps-query"
+alias xs="xbps-src"
+alias ls="ls -lhA"
