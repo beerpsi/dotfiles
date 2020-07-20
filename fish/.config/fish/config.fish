ae9ea94f628a52d7eb4a2670efdebf511dd6528a
beerpsi <vaythuonggialac@tutanota.com>
2020-07-20 13:15:46 +0700

    edits

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,22 @@
+# Add directory of scripts to PATH
 export PATH="$HOME/Downloads/git/void-packages:$HOME/.config/scripts:$PATH"
 
+
+# Alias for xbps-* (x*)
 alias xi="sudo xbps-install"
 alias xr="sudo xbps-remove"
 alias xq="xbps-query"
 alias xs="xbps-src"
+
+# Misc. alias
 alias ls="ls -lhA"
+
+# Alias for config files (cf*)
+alias cfa="vim $HOME/.config/alacritty/alacritty.yml"
+alias cffsh="vim $HOME/.config/fish/config.fish"
+alias cfi="vim $HOME/.config/i3/config"
+alias cfn="vim $HOME/.config/neofetch/config.conf"
+alias cfp="vim $HOME/.config/polybar/config"
+alias cfff="vim $HOME/dotfiles/firefox/.config/firefox/userChrome.css"
+
+
