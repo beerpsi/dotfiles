f0d84237b540ebb6b97e0090736e2439eb3bcf18
beerpsi <vaythuonggialac@tutanota.com>
2020-08-08 21:06:11 +0700

    Updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,17 +6,20 @@ set PATH "$HOME/Downloads/git/void-packages:$HOME/.config/scripts:$PATH"
 
 
 # Abbreviations for xbps-* (x*)
-abbr xi "doas -- xbps-install"
-abbr xr "doas -- xbps-remove"
+abbr xi "doas xbps-install"
+abbr xr "doas xbps-remove"
 abbr xq "xbps-query"
 abbr xs "xbps-src"
-abbr xsi "doas -- xbps-install --repository ~/Downloads/git/void-packages/hostdir/binpkgs"
-abbr xsi-nonfree "doas -- xbps-install --repository ~/Downloads/git/void-packages/hostdir/binpkgs/nonfree"
+abbr xsi "doas xbps-install --repository ~/Downloads/git/void-packages/hostdir/binpkgs"
+abbr xsi-nonfree "doas xbps-install --repository ~/Downloads/git/void-packages/hostdir/binpkgs/nonfree"
 abbr xsq "ls ~/Downloads/git/void-packages/srcpkgs/ | grep"
 
 # Misc. abbreviations
-abbr ls "ls -lhA"
-abbr doas "doas --"
+abbr la "ls -lhA"
+abbr v "vim"
+abbr theo "fortune theo"
+abbr wttr "curl -sSL v2.wttr.in/Thu_Dau_Mot"
+abbr wttr-lite "curl -sSL v2.wttr.in/Thu_Dau_Mot | grep \"Weather:\""
 
 # Abbreviations for config files (cf*)
 abbr cfa "vim $HOME/.config/alacritty/alacritty.yml"
@@ -27,4 +30,14 @@ abbr cfp "vim $HOME/.config/polybar/config"
 abbr cfff "vim $HOME/dotfiles/firefox/.config/firefox/userChrome.css"
 abbr cfv "vim $HOME/dotfiles/vim/.vimrc"
 abbr cfr "vim $HOME/dotfiles/rofi/.config/rofi/config.rasi"
-abbr cfs "vim $HOME/dotfiles/sxhkd/.config/sxhkd/sxhkdrc"    
+abbr cfs "vim $HOME/dotfiles/sxhkd/.config/sxhkd/sxhkdrc"   
+
+# doas insults for fish
+function doas
+    /usr/bin/doas $argv # I'll never use any other flags of doas anywayso why not, also because $argv doesn't get the --
+    if test "$status" = "1"
+        fortune theo 1>&2; false
+    else
+        /usr/bin/doas $argv
+    end
+end
