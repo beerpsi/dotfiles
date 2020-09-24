9da9e648d42da3582efe6e90ae19482b41e815aa
beerpsi <vaythuonggialac@tutanota.com>
2020-09-25 06:42:51 +0700

    Automated commit

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -86,7 +86,10 @@ function burn
 end
 
 function makemousework
-    xinput set-prop "Synaptics TM3253-001" "Coordinate Transformation Matrix" 3.2 0 0 0 3.2 0 0 0 0
+    xinput set-prop "Synaptics TM3253-001" "Coordinate Transformation Matrix" 5 0 0 0 5 0 0 0 0
+    xinput set-prop "Synaptics TM3253-001" "Accel Speed" 1
+    xinput set-prop "Synaptics TM3253-001" "Accel Profiles Enabled" 0, 1
+    xinput set-prop "Synaptics TM3253-001" "Disable While Typing Enabled" 0
     xinput set-prop "Logitech G PRO Gaming Mouse" "libinput Accel Profile Enabled" 0, 1
 end
 
